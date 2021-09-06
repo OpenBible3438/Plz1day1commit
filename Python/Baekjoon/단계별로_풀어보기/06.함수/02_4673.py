0181bd9ae046ada0b9c7b485569efb21069fe101
openbible <59015538+OpenBible3438@users.noreply.github.com>
2021-09-07 01:24:42 +0900

    함수

--- Python/Baekjoon/단계별로_풀어보기/06.함수/02_4673.py
+++ Python/Baekjoon/단계별로_풀어보기/06.함수/02_4673.py
@@ -1,19 +1,22 @@
 """
 셀프넘버
 """
-def selfNumber(n):
-    notSelfNumber = n
-    while (n == 0):
-        notSelfNumber += n % 10
+def d(n):
+    num = n
+    while True:
+        if (n == 0):
+            break
+        num += n % 10
         n = n // 10
-    return notSelfNumber
+    return num
 
-selfNumber_list = list()
+selfNumberList = [False for _ in range(10001)]
+
 for i in range(1, 10001):
-    notSelfNumber = selfNumber(i)
-    if notSelfNumber < 10001:
-        selfNumber_list.append(True)
+    notSelfNumber = d(i)
+    if notSelfNumber <= 10000:
+        selfNumberList[notSelfNumber] = True
 
-for i in range(selfNumber_list):
-    if not selfNumber_list[i]:
+for i in range(1, 10001):
+    if not selfNumberList[i]:
         print(i)
---
+++ Python/Baekjoon/단계별로_풀어보기/06.함수/03_1065.py
@@ -0,0 +1,25 @@
+"""
+한수
+"""
+def is_hansu(n):
+    result = False
+    if n < 100:
+        result = True
+    else:
+        n = str(n)
+        d = int(n[1]) - int(n[0])
+        for i in range(0, len(n)):
+            if i+1 == len(n): break
+            if int(n[i+1]) - int(n[i]) == d:
+                result = True
+            else:
+                result = False
+    return result
+
+N = int(input())
+
+count = 0
+for i in range(1, N+1):
+    if is_hansu(i):
+        count += 1
+print(count)

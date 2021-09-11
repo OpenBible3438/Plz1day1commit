5614c8456ecc404691c8fb6277ca57d8fbbcaa1b
openbible <59015538+OpenBible3438@users.noreply.github.com>
2021-09-11 23:38:47 +0900

    문자열

--- Python/Baekjoon/단계별로_풀어보기/07.문자열/05_1157.py
+++ Python/Baekjoon/단계별로_풀어보기/07.문자열/05_1157.py
@@ -1,3 +1,18 @@
 """
 단어 공부
 """
+
+word = input().upper()
+
+alphabetList = [ 0 for _ in range(26) ]
+
+for i in word:
+    alphabetList[ord(i)-65] += 1
+
+maxNumber = max(alphabetList)
+if alphabetList.count(maxNumber) > 1:
+    print('?')
+else:
+    for i in range(26):
+        if alphabetList[i] == maxNumber:
+            print(chr(i+65))
--- Python/Baekjoon/단계별로_풀어보기/07.문자열/test.py
+++
@@ -1,3 +0,0 @@
-testList = [0,0,0,0,1,2,3,4]
-
-print(testList.remove('0'))

2af9bd3d3e7b62399659a7542bccf232a30f12a9
openbible <59015538+OpenBible3438@users.noreply.github.com>
2021-09-15 00:37:06 +0900

    문자열

--- Python/Baekjoon/단계별로_풀어보기/07.문자열/10_1316.py
+++ Python/Baekjoon/단계별로_풀어보기/07.문자열/10_1316.py
@@ -2,3 +2,13 @@
 그룹 단어 체커
 """
 n = int(input())
+
+count = n
+for _ in range(n):
+    word = input()
+    for i in range(len(word)-1):
+        if word[i] != word[i+1]:
+            if word[i] in word[i+1:]:
+                count -= 1
+                break
+print(count)

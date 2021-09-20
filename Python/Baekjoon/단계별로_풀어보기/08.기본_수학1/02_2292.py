13a37e647368ce4a048f7056e0e75f904910c274
openbible <59015538+OpenBible3438@users.noreply.github.com>
2021-09-20 11:51:05 +0900

    기본수학1

--- Python/Baekjoon/단계별로_풀어보기/08.기본_수학1/02_2292.py
+++ Python/Baekjoon/단계별로_풀어보기/08.기본_수학1/02_2292.py
@@ -7,9 +7,9 @@ line = 1
 number = 1
 
 while True:
-    if number < N <= number+(6*line) and N == 1:
+    if (N==1) or (number < N < number+(6*line)):
         break
-    number += 6*line
     line += 1
+    number += (6*line)
 
-print(line) 
+print(line)

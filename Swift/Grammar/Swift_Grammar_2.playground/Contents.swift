ba9b2e891f1725fd0c154a969b4560d336d83283
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-01-03 23:34:56 +0900

    연산자 플레이그라운드 추가

---
+++ Swift/Grammar/Swift_Grammar_2.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+/* 연산자 */
+
+/*
+ 기본 연산자(Basic Operator)
+ + - * / %
+ */
+let a:Double = 20
+let b:Double = 30
+
+//let c = a + b => Int 타입을 나눌 경우, 딱 떨어지지 않는 이상 오류가 나기 쉽기 때문에 타입 지정을 해주자. 
+let c = a / b

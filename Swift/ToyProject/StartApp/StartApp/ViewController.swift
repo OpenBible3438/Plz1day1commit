b496d9a5165aa0777e52c0251a9775c20f23fe0d
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-03-05 23:28:50 +0900

    create toy project - StartApp
    스위프트 문법 대강 끝내구 앱 만들기 드디어 드디어 시작함 ㅋ

---
+++ Swift/ToyProject/StartApp/StartApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StartApp
+//
+//  Created by 편성경 on 2022/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

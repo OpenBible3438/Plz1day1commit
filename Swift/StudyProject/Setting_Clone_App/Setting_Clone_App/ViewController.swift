8589342b5256402e9e4622acdb59a1577b35dece
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-03-18 22:38:53 +0900

    Setting Cloine App 프로젝트 생성
    아이앱 기본 설정 앱 따라 만들기

---
+++ Swift/StudyProject/Setting_Clone_App/Setting_Clone_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Setting_Clone_App
+//
+//  Created by 편성경 on 2022/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

10625b756e8fef03d2282fde03664da82a86df0e
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-03-19 00:24:42 +0900

    TableView, TableViewCell 생성

---
+++ Swift/StudyProject/Setting_Clone_App/Setting_Clone_App/ProfileCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileCell.swift
+//  Setting_Clone_App
+//
+//  Created by 편성경 on 2022/03/18.
+//
+
+import UIKit
+
+class ProfileCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Swift/StudyProject/Setting_Clone_App/Setting_Clone_App/ViewController.swift
+++ Swift/StudyProject/Setting_Clone_App/Setting_Clone_App/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var settingTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        /*
+         추가한 ProfileCell (nib파일) 가져오기(등록).
+         storyboard에서 Identifier를 지정할 수 있는데 생성한 이름과 동일하게 짓는 것이 좋음
+         */
+        let profileNib = UINib(nibName: "ProfileCell", bundle: nil)
+        settingTableView.register(profileNib, forCellReuseIdentifier: "ProfileCell")
+        
     }
 
 
 }
 
+/*
+ TableView Protocol
+ */
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // numberOfRowsInSection는 몇 개의 cell을 구현할지를 나타내줘야 한다.
+        // Int 값을 리턴해야 되기 때문에 단순히 숫자를 써도 되지만
+        // CellCount 변수를 사용하는 것이 좋다.
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // cellForRowAt는 indexPath라는 순서를 통해서 어떠한 Cell을 보여줄 것인지 나타낸다.
+        // TableViewCell을 리턴해야 한다.
+        
+        // 제일 처음 만들었던 ProfileCell 객체 생성
+        let profileCell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath)
+        return profileCell
+    }
+    
+    
+}

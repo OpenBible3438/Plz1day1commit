549c283ebeb877bb9b34494ecec111c0b9d211d3
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-03-25 23:37:06 +0900

    setting clone app Cell 생성과 높이 설정

---
+++ Swift/StudyProject/Setting_Clone_App/Setting_Clone_App/MenuCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuCell.swift
+//  Setting_Clone_App
+//
+//  Created by 편성경 on 2022/03/24.
+//
+
+import UIKit
+
+class MenuCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code 
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Swift/StudyProject/Setting_Clone_App/Setting_Clone_App/ViewController.swift
+++ Swift/StudyProject/Setting_Clone_App/Setting_Clone_App/ViewController.swift
@@ -25,6 +25,10 @@ class ViewController: UIViewController {
         let profileNib = UINib(nibName: "ProfileCell", bundle: nil)
         settingTableView.register(profileNib, forCellReuseIdentifier: "ProfileCell")
         
+        // MenuCell 등록
+        // 위 profileNib 등록방법과 같지만 한 줄로 짧게 !
+        settingTableView.register(UINib(nibName: "MenuCell", bundle: nil), forCellReuseIdentifier: "MenuCell")
+        
     }
 
 
@@ -39,7 +43,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         // numberOfRowsInSection는 몇 개의 cell을 구현할지를 나타내줘야 한다.
         // Int 값을 리턴해야 되기 때문에 단순히 숫자를 써도 되지만
         // CellCount 변수를 사용하는 것이 좋다.
-        return 1
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -47,9 +51,30 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         // TableViewCell을 리턴해야 한다.
         
         // 제일 처음 만들었던 ProfileCell 객체 생성
+        /*
         let profileCell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath)
         return profileCell
+         */
+        
+        if indexPath.row == 0 {
+            // 0 번째 row
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath)
+            return cell
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath)
+        return cell
     }
     
+    // 높이를 지정할 때 사용하는 함수
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        // 각 셀마다 높이 지정
+        if indexPath.row == 0 {
+            // 첫 번째는 profileCell이니까 스토리보드에서 지정한 높이대로 자동 설정
+            return UITableView.automaticDimension
+        }
+        
+        // 나머지 셀들은 다 일정한 높이 지정
+        return 60
+    }
     
 }

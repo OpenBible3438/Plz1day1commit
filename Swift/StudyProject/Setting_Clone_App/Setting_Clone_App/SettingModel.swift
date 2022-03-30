6b7a3d99379720cc98e5fff5291b6c605fd5a11d
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-03-30 23:21:59 +0900

    Setting Clone App Cell 변경

--- Swift/StudyProject/Setting_Clone_App/Setting_Clone_App/SettingModel.swift
+++ Swift/StudyProject/Setting_Clone_App/Setting_Clone_App/SettingModel.swift
@@ -9,6 +9,15 @@ import Foundation
 
 // struct 구조
 struct SettingModel {
+    /*
+     첫 번째 섹션에 사용자의 정보가 들어가는 부분은 subTitle이 들어가지만
+     나머지 섹션에서는 subTitle이 있지않고 menuTitle만 존재한다.
+     
+     그리고 첫 번째 섹션에는 오른쪽에 화살표 이미지가 없지만
+     나머지 섹션에는 오른쪽에 화살표 이미지가 있다
+     
+     => subTitle, rightImageName은 optional로 생성
+     */
     var leftImageName: String = ""
     var menuTitle: String = ""
     var subTitle: String?
--- Swift/StudyProject/Setting_Clone_App/Setting_Clone_App/ViewController.swift
+++ Swift/StudyProject/Setting_Clone_App/Setting_Clone_App/ViewController.swift
@@ -8,15 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     // SettingModel
+    /*
+     첫 번째 섹션에 1개
+     두 번째 섹션에 3개
+     세 번째 섹션에 ~개
+     등등을 담기 위한 2중 배열 구조로 생성
+     */
     var settingModel = [[SettingModel]]()
 
     @IBOutlet weak var settingTableView: UITableView!
     
     func makeData() {
-        // 초기값 설정
-        SettingModel(leftImageName: <#T##String#>, menuTitle: <#T##String#>, subTitle: <#T##String?#>, rightImageName: <#T##String?#>)
+        // Section 1
+        settingModel.append(
+        [SettingModel(leftImageName: "person.circle", menuTitle: "Sign in to your iPhone", subTitle: "Set up iCloud, the App Store, and more.", rightImageName: nil)]
+        )
+        
+        // Section 2
+        settingModel.append(
+        [SettingModel(leftImageName: "gear", menuTitle: "General", subTitle: nil, rightImageName: "chevron.right"),
+        SettingModel(leftImageName: "figure.wave", menuTitle: "Accessibility", subTitle: nil, rightImageName: "chevron.right"),
+        SettingModel(leftImageName: "hand.raised.square.fill", menuTitle: "Privacy", subTitle: nil, rightImageName: "chevron.right")]
+        )
     }
     
     override func viewDidLoad() {
@@ -37,6 +52,11 @@ class ViewController: UIViewController {
         // 위 profileNib 등록방법과 같지만 한 줄로 짧게 !
         settingTableView.register(UINib(nibName: "MenuCell", bundle: nil), forCellReuseIdentifier: "MenuCell")
         
+        // TableView 배경색 변경
+        settingTableView.backgroundColor = UIColor(red: 0.835, green: 0.851, blue: 0.878, alpha: 1.000)
+        
+        makeData()
+        
     }
 
 
@@ -51,7 +71,18 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         // numberOfRowsInSection는 몇 개의 cell을 구현할지를 나타내줘야 한다.
         // Int 값을 리턴해야 되기 때문에 단순히 숫자를 써도 되지만
         // CellCount 변수를 사용하는 것이 좋다.
-        return 5
+        //return 5
+        
+        // 3/30
+        /*
+         makeData()에서 구현한 settingModel
+         각 섹션의 들어있는 개수만큼 출력되게 작성
+         */
+        return settingModel[section].count
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // 섹션의 개수
+        return settingModel.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -64,12 +95,28 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         return profileCell
          */
         
-        if indexPath.row == 0 {
+        if indexPath.section == 0 {
             // 0 번째 row
-            let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath) as! ProfileCell
+            
+            // id 이름 "ProfileCell"을 가져온 것을 as! ProfileCell를 추가하여 강제적으로 TypeCasting
+            
+            cell.topTitle.text = settingModel[indexPath.section][indexPath.row].menuTitle
+            cell.profileImageView.image = UIImage(systemName:settingModel[indexPath.section][indexPath.row].leftImageName)
+            cell.bottomDescription.text = settingModel[indexPath.section][indexPath.row].subTitle
+            
             return cell
         }
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath)
+        
+        // 1번째 row(cell)부터는 MenuCell의 형태를 가짐
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath) as! MenuCell
+        cell.leftImageView.image = UIImage(systemName: settingModel[indexPath.section][indexPath.row].leftImageName)
+        cell.leftImageView.tintColor = UIColor(red: 0.004, green: 0.118, blue: 0.220, alpha: 1.000)
+        print("print color code",cell.leftImageView.tintColor.description)
+        cell.middleTitle.text = settingModel[indexPath.section][indexPath.row].menuTitle
+        cell.rightImageView.image = UIImage(systemName: settingModel[indexPath.section][indexPath.row].rightImageName ?? "" ) // optional 올 수 없어서 nil일 경우 "" 빈 스트링 넣는 코드 추가
+
+        
         return cell
     }
     

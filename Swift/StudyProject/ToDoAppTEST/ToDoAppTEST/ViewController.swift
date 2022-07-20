533e97a4086fb786285c83b3707d95963e772aa9
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-07-20 22:23:42 +0900

    Toolbar 생성 & item 정렬

--- Swift/StudyProject/ToDoAppTEST/ToDoAppTEST/ViewController.swift
+++ Swift/StudyProject/ToDoAppTEST/ToDoAppTEST/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Toolbar 추가 item
+    @IBOutlet weak var addListBtn: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // 추가 item 이벤트
+    @IBAction func addList(_ sender: Any) {
+        
+    }
+    
 }
 

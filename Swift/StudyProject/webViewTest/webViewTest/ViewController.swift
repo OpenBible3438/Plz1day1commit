854f01d7d38aa89e0db306ea90a5f6c6420eaa90
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-08-02 00:08:44 +0900

    webview 화면 띄우기

--- Swift/StudyProject/webViewTest/webViewTest/ViewController.swift
+++ Swift/StudyProject/webViewTest/webViewTest/ViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainWebview: WKWebView!
+    
+    /*
+     loadView()를 override하여 webView를 할당한다.
+     ViewController가 가지고 있는 view를 다른 view로 지정하고 싶을 때 사용한다.
+     
+     보통 viewDidLoad()에서 view를 초기화해서 사용하지만
+     webview의 경우에는 viewController내에서 바로 초기화하여 사용하는 게 효율적.
+     */
+    override func loadView() {
+        super.loadView()
+        
+        self.view = self.mainWebview
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let url = URL(string: "http://localhost:8080/") {
+            let request = URLRequest(url: url)
+            mainWebview.load(request)
+        }
     }
 
 

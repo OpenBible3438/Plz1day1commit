6a5c929356e1f3251b9effb0b57e4d17f17cd115
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-08-23 18:19:39 +0900

    Firestore에서 오늘 날짜 데이터 불러오기

--- Swift/StudyProject/CalendarMemo/CalendarMemo/main/ViewController.swift
+++ Swift/StudyProject/CalendarMemo/CalendarMemo/main/ViewController.swift
@@ -7,24 +7,47 @@
 
 import UIKit
 import FSCalendar
+import FirebaseFirestore
 
-class ViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource {
+class ViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
 
     @IBOutlet weak var calendarView: FSCalendar!
     @IBOutlet weak var textView: UITextView!
     
+    // Firebase
+    let db = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        textView.text = ""
         setCalendarUI()
         
-        /* TextView */
-        textView.text = "작성된 글이 없습니다."
+        // 오늘 날짜 String
+        let nowDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd" // Firestore 문서ID
+        let todayString = dateFormatter.string(from: nowDate)
+        
+        // 오늘 날짜에 해당하는 문서 찾기
+        let docRef = db.collection("MEMO").document(todayString)
+        docRef.getDocument() { (document, error) in
+            if let document = document, document.exists {
+                // 해달 날짜의 doc 존재
+                let todayContents = document.data()!["contents"] as! String
+                print(todayContents)
+                self.textView.text = todayContents
+            } else {
+                print("document does not exist")
+                self.textView.text = "작성된 글이 없습니다"
+            }
+        }
+        
     }
     
+    
+    /* FSCalendar UI 설정 */
     func setCalendarUI() {
-        /* FSCalendar */
         self.calendarView.delegate = self
         self.calendarView.dataSource = self
         

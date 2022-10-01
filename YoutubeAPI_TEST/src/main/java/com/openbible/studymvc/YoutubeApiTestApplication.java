8e0a7f2c390d09f7c109cf4894dfec39a2da451e
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-10-01 22:47:31 +0900

    create Project

---
+++ YoutubeAPI_TEST/src/main/java/com/openbible/studymvc/YoutubeApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.openbible.studymvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeApiTestApplication.class, args);
+	}
+
+}
---
+++ YoutubeAPI_TEST/src/test/java/com/openbible/studymvc/YoutubeApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openbible.studymvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
